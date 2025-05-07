7f1b260c3780eb2a7261d784230ff0c8cde5566b
Hemu585 <121238521+Hemu585@users.noreply.github.com>
2025-05-07 12:01:58 +0000

    check

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+/*let score = "33abc"
 
 console.log(typeof score);// string
 console.log(typeof (score));// string
@@ -13,13 +13,25 @@ console.log(valueInNumber); //NaN
  true = 1, false = 0
 "hemang" = NaN (not a number) , but type is number
 */
-let isLoggedIn = 1
+/*let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedin)
 console.log(booleanIsLoggedIn) // => true
 /* isLoggedIn = "" => false
 isLoggedIn = "hemang" => true*/
 
-let someNumber = 33
+/*let someNumber = 33
 let stringNumber = String(someNumber);
 console.log(stringNumber);// 33
 console.log(typeof stringNumber);// String
+*/
+
+// ************Operations********** 
+ 
+let value = 3
+let negvalue =  - value;
+
+// console.log(2+2)
+//console.log(2*2)  => multiply
+// console.log(2**3) => 2^3
+
+console.log(negvalue)
