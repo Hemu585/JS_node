5be9622fb4a511abfa9920cbc467afc5478d9c76
Hemu585 <121238521+Hemu585@users.noreply.github.com>
2025-05-07 11:48:54 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33abc"
+
+console.log(typeof score);// string
+console.log(typeof (score));// string
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); //NaN
+/* null => 0
+ undefined => NaN
+ "33" = 33
+ "33abc" = NaN
+ true = 1, false = 0
+"hemang" = NaN (not a number) , but type is number
+*/
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedin)
+console.log(booleanIsLoggedIn) // => true
+/* isLoggedIn = "" => false
+isLoggedIn = "hemang" => true*/
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);// 33
+console.log(typeof stringNumber);// String
