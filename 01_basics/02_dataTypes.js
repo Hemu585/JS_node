2d23b595eee85778984efb0a968f6812b9526887
Hemu585 <121238521+Hemu585@users.noreply.github.com>
2025-05-07 11:32:49 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) // dont use as we are using nodejs, not browser
+// code readability should be high 
+
+let name = "hemang" // string => ""
+let age = 21 // number => 2 to power 53
+let isLoggedin =  false // boolean => true/false
+let state;
+// null => standalone value 
+// undefined => 
+    //symbol => unique
+// object
+console.log(typeof age) // number
+console.log(typeof null)// object
+console.log(typeof undefined)// undefined
